0f5bbd66275a65d2d5eb496f61b75b133fe3e671
andr3ws <core@anworks.de>
2011-09-05 20:30:57 +0200

    Make some inline Comments

--- CoreCMS/core/install/functions/installer.active.php
+++ CoreCMS/core/install/functions/installer.active.php
@@ -1,3 +1,10 @@
 <?
-		$installerActive = false; 
-		?>
+  /* 
+   * We check this var in index.php file, to start the install script at the first run of core 
+   *
+   * @param: is true if core havn't installed before 
+   *         is false if Core is succesfully installed
+   */
+  
+		$installerActive = true; 
+?>
