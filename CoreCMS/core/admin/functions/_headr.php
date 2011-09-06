db3cbd7504704b11f3829701ea91ff13bb669125
Andreas Klein <core@anworks.de>
2011-09-06 10:48:37 +0300

    moved session_start(); function to admin_session.php

--- CoreCMS/core/admin/functions/_headr.php
+++ CoreCMS/core/admin/functions/_headr.php
@@ -5,7 +5,11 @@ $root = set_root();
 require($root . "user/configuration.php");
 $debug_str = "";
 $fatal = false;
-session_start();
+
+/*
+ *  moved to login function in admin_session.php
+ *  session_start();
+ */
 
 $connection = @mysql_connect($db_server, $db_user, $db_pass);
 @mysql_select_db($db_name, $connection);
